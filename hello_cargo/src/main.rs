ecd433b616c0cbc29d77139f6c3d0fd395a02a9c
1pass-fi <charlesward19744@gmail.com>
2023-11-27 03:59:00 -0600

    Boolean values

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,21 +1,25 @@
 fn main() {
-    let spaces = "  ";
-    let spaces = spaces.len();
-    println!("Length: {}", spaces);
-    let guess: i16 = "42".parse().expect("");
-    println!("Guess: {}", guess);
+    let t = true;
+    let t2 = false;
+    let f: bool = false;
+    println!("Boolean values: {}, {f}, {}", t, t2);
+    // let spaces = "  ";
+    // let spaces = spaces.len();
+    // println!("Length: {}", spaces);
+    // let guess: i16 = "42".parse().expect("");
+    // println!("Guess: {}", guess);
 
-    let x = 2.0;
-    let y: f32 = 3.0;
-    println!("Floating numbers: {}, {}", x, y);
+    // let x = 2.0;
+    // let y: f32 = 3.0;
+    // println!("Floating numbers: {}, {}", x, y);
 
-    let sum = 5 + 10;
-    let diff = 95.5 - 4.3;
-    let product = 4 * 30;
-    let quotient = 56.7 / 32.2;
-    let truncated = -5 / 3;
-    let remainder = 43 % 5;
-    println!("New values: {}, {}, {}, {}, {}, {}", sum, diff, product, quotient, truncated, remainder);
+    // let sum = 5 + 10;
+    // let diff = 95.5 - 4.3;
+    // let product = 4 * 30;
+    // let quotient = 56.7 / 32.2;
+    // let truncated = -5 / 3;
+    // let remainder = 43 % 5;
+    // println!("New values: {}, {}, {}, {}, {}, {}", sum, diff, product, quotient, truncated, remainder);
     // let mut x = 5;
     // println!("The value of x is: {x}");
     // x = 6;
