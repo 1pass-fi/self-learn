db43429e1e40ca86ce20184f5abaea8b90cba524
1pass-fi <charlesward19744@gmail.com>
2023-11-27 03:50:37 -0600

    Float numbers

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,17 +1,34 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is {x}");
+    let spaces = "  ";
+    let spaces = spaces.len();
+    println!("Length: {}", spaces);
+    let guess: i16 = "42".parse().expect("");
+    println!("Guess: {}", guess);
 
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    println!("Three hours value in seconds: {}", THREE_HOURS_IN_SECONDS);
+    let x = 2.0;
+    let y: f32 = 3.0;
+    println!("Floating numbers: {}, {}", x, y);
 
-    let x = x + 1;
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+    let sum = 5 + 10;
+    let diff = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3;
+    let remainder = 43 % 5;
+    println!("New values: {}, {}, {}, {}, {}, {}", sum, diff, product, quotient, truncated, remainder);
+    // let mut x = 5;
+    // println!("The value of x is: {x}");
+    // x = 6;
+    // println!("The value of x is {x}");
+
+    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    // println!("Three hours value in seconds: {}", THREE_HOURS_IN_SECONDS);
+
+    // let x = x + 1;
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {x}");
+    // }
     
-    println!("The value of x is: {x}");
+    // println!("The value of x is: {x}");
 }
