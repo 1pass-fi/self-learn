7d6b3d3bf48c7443910ce5b89905046ea69cde9f
1pass-fi <charlesward19744@gmail.com>
2023-11-27 03:37:32 -0600

    Variables and Mutability

---
+++ hello_cargo/target/debug/deps/hello_cargo-ad935257d28a0fae.d
@@ -0,0 +1,5 @@
+/home/ket/Documents/Rust/self-learn/hello_cargo/target/debug/deps/hello_cargo-ad935257d28a0fae: src/main.rs
+
+/home/ket/Documents/Rust/self-learn/hello_cargo/target/debug/deps/hello_cargo-ad935257d28a0fae.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/ket/Documents/Rust/self-learn/hello_cargo/target/debug/hello_cargo: /home/ket/Documents/Rust/self-learn/hello_cargo/src/main.rs
