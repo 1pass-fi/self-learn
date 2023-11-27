0a9f23ecb3007af581b61f3e54050037b3e03f9c
1pass-fi <charlesward19744@gmail.com>
2023-11-27 10:54:17 -0600

    Control Flow

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,23 +1,79 @@
-fn another_function(x: i32, y: char) {
-    println!("Another function {x}{y}");
-}
-
-fn five(x: i32) -> i32 {
-    1 + x + 1
-}
-
 fn main() {
-    println!("hello, world");
-    another_function(2, 'z');
+    for number in (4..1).rev() {
+        println!("{number}");
+    }
+    println!("liftoff");
+    // let a = [10, 20, 30, 40, 50];
+    // for element in a {
+    //     println!("value is: {element}");
+    // }
+    // let mut index = 0;
+    // while index < 5 {
+    //     println!("the value is {}", a[index]);
+    //     index = index + 2;
+    // }
+    // let mut number = 3;
+    // while number != 0 {
+    //     println!("{number}");
+    //     number -= 1;
+    // }
+    // println!("Liftoff!");
+    // let mut count = 0;
+    // 'counting_up: loop {
+    //     println!("count = {count}");
+    //     let mut remaining = 10;
+    //     loop {
+    //         println!("remaining = {remaining}");
+    //         if remaining == 9 {
+    //             break;
+    //         }
+    //         if count == 2 {
+    //             break 'counting_up;
+    //         }
+    //         remaining -= 1;
+    //     }
+    //     count += 1;
+    // }
+    // println!("End count = {count}");
+    // let mut counter = 0;
+    // let result = loop {
+    //     counter += 1;
+    //     if counter == 10 {
+    //         break counter * 2;
+    //     }
+    // };
+    // println!("result: {result}")
+    // let condition = true;
+    // let number = if condition {5} else {6};
+    // println!("{number}");
+    // if number < 5 {
+    //     println!("Condition was true");
+    // } else {
+    //     println!("Condition was false");
+    // }
+    // if number % 4 == 0 {
+    //     println!("divisible by 4");
+    // } else if number % 3 == 0 {
+    //     println!("divisible by 3");
+    // } else if number % 3 == 0 {
+    //     println!("divisible by 2");
+    // } else {
+    //     println!("not divisible by 4, 3, or 2");
+    // }
+    
 
-    let x = {
-        let y = 6;
-        y + 1
-    };
-    println!("{}", x);
 
-    let x = five(3);
-    println!("The return value: {}", x);
+    // println!("hello, world");
+    // another_function(2, 'z');
+
+    // let x = {
+    //     let y = 6;
+    //     y + 1
+    // };
+    // println!("{}", x);
+
+    // let x = five(3);
+    // println!("The return value: {}", x);
     // let t = true;
     // let t2 = false;
     // let f: bool = false;
@@ -89,3 +145,11 @@ fn main() {
     
     // println!("The value of x is: {x}");
 }
+
+// fn another_function(x: i32, y: char) {
+//     println!("Another function {x}{y}");
+// }
+
+// fn five(x: i32) -> i32 {
+//     1 + x + 1
+// }
