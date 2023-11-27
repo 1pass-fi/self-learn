8b77749d51e5a352226a1a1034f4cf367fb38acf
1pass-fi <charlesward19744@gmail.com>
2023-11-27 03:40:40 -0600

    Shadowing, Constants

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -3,4 +3,15 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is {x}");
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("Three hours value in seconds: {}", THREE_HOURS_IN_SECONDS);
+
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+    
+    println!("The value of x is: {x}");
 }
