c0d0ea8abdd125843510f16e2ac3d29a0c8a47c4
1pass-fi <charlesward19744@gmail.com>
2023-12-12 04:00:31 -0600

    What is ownership

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,19 +1,28 @@
 // use rand::Rng;
-fn gives_ownership() -> String {
-    let some_string = String::from("yours");
-    some_string
-}
+// fn gives_ownership() -> String {
+//     let some_string = String::from("yours");
+//     some_string
+// }
 
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+// fn takes_and_gives_back(a_string: String) -> String {
+//     a_string
+// }
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
 
 fn main() {
-    let s1 = gives_ownership();
-    let s2 = String::from("hello");
-    let s3 = takes_and_gives_back(s2);
-    println!("{}", s1);
-    println!("{}", s3);
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+    println!("The length of '{}' is {}", s2, len);
+
+    // let s1 = gives_ownership();
+    // let s2 = String::from("hello");
+    // let s3 = takes_and_gives_back(s2);
+    // println!("{}", s1);
+    // println!("{}", s3);
 
     // let s = String::from("hello");
     // takes_ownership(s);
