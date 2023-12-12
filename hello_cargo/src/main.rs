f5cccec90399e301a9202ed06d609d09a305ef9b
1pass-fi <charlesward19744@gmail.com>
2023-12-12 07:57:32 -0600

    References and Borrowing

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -8,15 +8,26 @@
 //     a_string
 // }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len();
-    (s, length)
-}
+// fn calculate_length(s: String) -> (String, usize) {
+//     let length = s.len();
+//     (s, length)
+// }
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
 
 fn main() {
-    let s1 = String::from("hello");
-    let (s2, len) = calculate_length(s1);
-    println!("The length of '{}' is {}", s2, len);
+    // let mut s = String::from("hello");
+    // let r1 = &s;
+    // let r2 = &s;
+    // println!("{} and {}", r1, r2);
+    // let r3 = &mut s;
+    // println!("{}s", r3);
+    // let mut s = String::from("hello");
+    // change(&mut s);
+    // let s1 = String::from("hello");
+    // let (s2, len) = calculate_length(s1);
+    // println!("The length of '{}' is {}", s2, len);
 
     // let s1 = gives_ownership();
     // let s2 = String::from("hello");
